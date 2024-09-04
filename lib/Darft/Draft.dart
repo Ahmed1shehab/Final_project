2597a8751775370f03c96fc0564aded9c514cd36
Mohab Rajab <ahmed.shehab.7355@gmail.com>
2024-09-04 20:38:19 +0300

    darft folder

---
+++ lib/Darft/Draft.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Draft extends StatelessWidget {
+  const Draft({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
