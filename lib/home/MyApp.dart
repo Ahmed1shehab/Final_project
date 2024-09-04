71d27e398caa41d09c695be2cf44665329211408
Ahmed shehab <Ahmed.shehab.7355@gmail.com>
2024-09-05 02:52:21 +0300

    Home page

---
+++ lib/home/MyApp.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+}
